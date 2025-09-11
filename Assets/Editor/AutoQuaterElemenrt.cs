ba909f6024ea2a807a22705e0e70213468af5d81
popoteAtelier <yvan.bladet@gmail.com>
2025-09-11 14:56:56 +0200

    Editor Window Auto Quarter ready

--- Assets/Editor/AutoQuaterElemenrt.cs
+++ Assets/Editor/AutoQuaterElemenrt.cs
@@ -1,6 +1,9 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
 using script;
 using UnityEngine;
+using Debug = UnityEngine.Debug;
 using Random = UnityEngine.Random;
 
 public static class AutoQuaterElement
@@ -10,6 +13,7 @@ public static class AutoQuaterElement
     }
     public class CellAutoQuarter {
         public Vector2Int Pos;
+        public Vector2Int PropertyPlace;
         public AutoQuarterCellType Type;
         public TestCell TestCell;
 
@@ -21,7 +25,11 @@ public static class AutoQuaterElement
         public CellAutoQuarter[,] Cell;
         
         private EditorWindowQuartierAutoPlacer _autoPlacerManager;
-        private CellAutoQuarter _centerCell;
+        public CellAutoQuarter CenterCell;
+        public Vector2Int DirectionToRoad;
+        public List<CellAutoQuarter> BorderCells= new List<CellAutoQuarter>();
+        public List<CellAutoQuarter> PassCells = new List<CellAutoQuarter>();
+        
         public int GetWight => Cell.GetLength(0);
         public int GetHeight => Cell.GetLength(1);
         public bool CanBeDividedOnX(int minsize) {
@@ -31,6 +39,19 @@ public static class AutoQuaterElement
         public bool CanBeDividedOnY(int minsize) {
             if (GetHeight < minsize * 2) return false;
             return true;
+        }
+
+        public CellAutoQuarter[] Get8Neighbours(CellAutoQuarter cell) {
+            List<CellAutoQuarter> neighbours = new List<CellAutoQuarter>();
+            if( GetCell(cell.PropertyPlace+new Vector2Int(1,0))!=null) neighbours.Add(GetCell(cell.PropertyPlace+new Vector2Int(1,0)));
+            if( GetCell(cell.PropertyPlace+new Vector2Int(1,1))!=null) neighbours.Add(GetCell(cell.PropertyPlace+new Vector2Int(1,1)));
+            if( GetCell(cell.PropertyPlace+new Vector2Int(0,1))!=null) neighbours.Add(GetCell(cell.PropertyPlace+new Vector2Int(0,1)));
+            if( GetCell(cell.PropertyPlace+new Vector2Int(-1,1))!=null) neighbours.Add(GetCell(cell.PropertyPlace+new Vector2Int(-1,1)));
+            if( GetCell(cell.PropertyPlace+new Vector2Int(-1,0))!=null) neighbours.Add(GetCell(cell.PropertyPlace+new Vector2Int(-1,0)));
+            if( GetCell(cell.PropertyPlace+new Vector2Int(-1,-1))!=null) neighbours.Add(GetCell(cell.PropertyPlace+new Vector2Int(-1,-1)));
+            if( GetCell(cell.PropertyPlace+new Vector2Int(0,-1))!=null) neighbours.Add(GetCell(cell.PropertyPlace+new Vector2Int(0,-1)));
+            if( GetCell(cell.PropertyPlace+new Vector2Int(1,-1))!=null) neighbours.Add(GetCell(cell.PropertyPlace+new Vector2Int(1,-1)));
+            return neighbours.ToArray();
         }
 
         public void SetCellType(EditorWindowQuartierAutoPlacer AutoPlacer) {
@@ -63,7 +84,7 @@ public static class AutoQuaterElement
 
         private void SetCenterCell() {
             Cell[GetWight / 2, GetHeight / 2].Type = AutoQuarterCellType.House;
-            _centerCell = Cell[GetWight / 2, GetHeight / 2];
+            CenterCell = Cell[GetWight / 2, GetHeight / 2];
         }
 
         private void SetBorders() {
@@ -76,6 +97,7 @@ public static class AutoQuaterElement
                             continue;
                         }
                         Cell[x, y].Type = AutoQuarterCellType.Borders;
+                        BorderCells.Add(Cell[x,y]);
                     } 
                 }
             }
@@ -86,17 +108,17 @@ public static class AutoQuaterElement
 
             Vector2Int additionalCell = Vector2Int.zero;
             Vector2Int bestVector = new Vector2Int(1,0);
-            int _bestDitance = workingZoneSize.x - _centerCell.Pos.x;
+            int _bestDitance = workingZoneSize.x - CenterCell.Pos.x;
 
-            if (_bestDitance > workingZoneSize.y - _centerCell.Pos.y); {
-                _bestDitance = workingZoneSize.y - _centerCell.Pos.y;
+            if (_bestDitance > workingZoneSize.y - CenterCell.Pos.y) {
+                _bestDitance = workingZoneSize.y - CenterCell.Pos.y;
                 bestVector = new Vector2Int(0, 1);
             }
-            if (_bestDitance > _centerCell.Pos.y); {
-                _bestDitance = _centerCell.Pos.y;
+            if (_bestDitance > CenterCell.Pos.y) {
+                _bestDitance = CenterCell.Pos.y;
                 bestVector = new Vector2Int(0, -1);
             }
-            if (_bestDitance >  _centerCell.Pos.x); {
+            if (_bestDitance >  CenterCell.Pos.x) {
                 bestVector = new Vector2Int(-1, 0);
             }
 
@@ -109,15 +131,123 @@ public static class AutoQuaterElement
                 else additionalCell = new Vector2Int(0, -1);
             }
 
+            DirectionToRoad = bestVector;
             bool isComplet = false;
-            int i = 1;
+            int i = 0;
             while (!isComplet) {
-                
-                
+                i++;
+                Vector2Int newPos = CenterCell.PropertyPlace + (bestVector * i);
+                if (GetCell(newPos) != null) {
+                    if (GetCell(newPos).Type == AutoQuarterCellType.Borders) BorderCells.Remove(GetCell(newPos));
+                    GetCell(newPos).Type = AutoQuarterCellType.Pass;
+                    PassCells.Add(GetCell(newPos));
+                    
+                }
+                else {
+                    isComplet = true;
+                }
+                if (GetCell(newPos+additionalCell) != null) {
+                    if (GetCell(newPos).Type == AutoQuarterCellType.Borders) BorderCells.Remove(GetCell(newPos));
+                    GetCell(newPos+additionalCell).Type = AutoQuarterCellType.Pass;
+                    PassCells.Add(GetCell(newPos));
+                }
+
+                if (i > 1000) {
+                    Debug.LogWarning("AutoQuater Property Pass calculation does over 1000 pass and had been stop" );
+                    isComplet = true;
+                }
+            }
+        }
+
+        private List<CellAutoQuarter> GetNeighboursCellOfType(CellAutoQuarter cell, AutoQuarterCellType type) {
+            List<CellAutoQuarter> neighbours = new List<CellAutoQuarter>();
+            if (GetCell(cell.PropertyPlace + new Vector2Int(1, 0)) != null &&
+                GetCell(cell.PropertyPlace + new Vector2Int(1, 0)).Type == type) {
+                neighbours.Add(GetCell(cell.PropertyPlace + new Vector2Int(1, 0)));
+            } 
+            if (GetCell(cell.PropertyPlace + new Vector2Int(-1, 0)) != null &&
+                GetCell(cell.PropertyPlace + new Vector2Int(-1, 0)).Type == type) {
+                neighbours.Add(GetCell(cell.PropertyPlace + new Vector2Int(-1, 0)));
+            } 
+            if (GetCell(cell.PropertyPlace + new Vector2Int( 0,1)) != null &&
+                GetCell(cell.PropertyPlace + new Vector2Int( 0,1)).Type == type) {
+                neighbours.Add(GetCell(cell.PropertyPlace + new Vector2Int(0,1)));
+            } 
+            if (GetCell(cell.PropertyPlace + new Vector2Int(0,-1)) != null &&
+                GetCell(cell.PropertyPlace + new Vector2Int(0,-1)).Type == type) {
+                neighbours.Add(GetCell(cell.PropertyPlace + new Vector2Int(0,-1)));
             }
 
-
+            return neighbours;
         }
-        
+
+        public CellAutoQuarter[] GetPalissadeKeyCells() { 
+            CellAutoQuarter currentCell = GetFirstCellBorder();
+            if (currentCell == null) return null;
+            List<CellAutoQuarter> keycells = new List<CellAutoQuarter>();
+            keycells.Add(currentCell);
+            int i = 0;
+            List<CellAutoQuarter> neighbours;
+            CellAutoQuarter previewsCell=null;
+            Vector2Int currentDir =
+                GetNeighboursCellOfType(currentCell, AutoQuarterCellType.Borders)[0].PropertyPlace - currentCell.PropertyPlace;
+            while (i<100) {
+                i++;
+                if (GetCell(currentCell.PropertyPlace + currentDir)!=null&&GetCell(currentCell.PropertyPlace + currentDir).Type == AutoQuarterCellType.Borders) {
+                    previewsCell = currentCell;
+                    currentCell = GetCell(currentCell.PropertyPlace + currentDir);
+                    continue;
+                }
+                neighbours=GetNeighboursCellOfType(currentCell, AutoQuarterCellType.Borders);
+                if (neighbours.Count < 2) {
+                    keycells.Add(currentCell);
+                    return keycells.ToArray();
+                }
+                foreach (var neighbour in neighbours) {
+                    if (neighbour == previewsCell) continue;
+                    keycells.Add(currentCell);
+                    currentDir = neighbour.PropertyPlace - currentCell.PropertyPlace;
+                    previewsCell = currentCell;
+                    currentCell = neighbour;
+                    break;
+                }
+            }
+            Debug.LogWarning("Get Palissade Key Cell Got Stop After over 1000loops");
+            return keycells.ToArray();
+        }
+
+        private CellAutoQuarter GetFirstCellBorder() {
+            foreach (var cell in BorderCells) {
+                if (GetNeighboursCellOfType(cell,AutoQuarterCellType.Borders).Count==1) return cell;
+            }
+            return null;
+        }
+
+        public CellAutoQuarter GetRandomPassCell() {
+            return PassCells[Random.Range(0, PassCells.Count)];
+        }
+
+        public List<CellAutoQuarter> GetFreeCells() {
+            List<CellAutoQuarter> freelist = new List<CellAutoQuarter>();
+            foreach (var cell in Cell) {
+                if (cell.Type  ==AutoQuarterCellType.Free) freelist.Add(cell);
+            }
+            return freelist;
+        }
+
+        public void SetHouseCells() {
+            foreach (var cell in Get8Neighbours(CenterCell)) {
+                if (cell.Type == AutoQuarterCellType.Borders) BorderCells.Remove(cell);
+                if (cell.Type == AutoQuarterCellType.Pass) PassCells.Remove(cell);
+                cell.Type = AutoQuarterCellType.House;
+            }
+        }
+
+        public void SetBorderCellsToFree() {
+            foreach (var cell in BorderCells) {
+                cell.Type = AutoQuarterCellType.Free;
+            }
+            BorderCells.Clear();
+        }
     }  
 }
